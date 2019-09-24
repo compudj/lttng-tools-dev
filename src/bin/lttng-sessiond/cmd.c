d0f4da61c38475f4c2e5500424c56c6976abaef6
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2019-09-24 19:44:35 -0400

    fix wip

--- src/bin/lttng-sessiond/cmd.c
+++ src/bin/lttng-sessiond/cmd.c
@@ -4896,6 +4896,13 @@ int cmd_rotate_session(struct ltt_session *session,
 		}
         }
 
+	ret = lttng_trace_chunk_close_prepare(session->current_trace_chunk,
+		command);
+	if (ret) {
+		cmd_ret = LTTNG_ERR_CLOSE_TRACE_CHUNK_FAIL_CONSUMER;
+		goto error;
+	}
+
 	/*
 	 * The current trace chunk becomes the chunk being archived.
 	 *
@@ -4914,13 +4921,6 @@ int cmd_rotate_session(struct ltt_session *session,
 	chunk_status = lttng_trace_chunk_get_id(chunk_being_archived,
 			&ongoing_rotation_chunk_id);
 	assert(chunk_status == LTTNG_TRACE_CHUNK_STATUS_OK);
-
-	ret = lttng_trace_chunk_close_prepare(chunk_being_archived,
-		command);
-	if (ret) {
-		cmd_ret = LTTNG_ERR_CLOSE_TRACE_CHUNK_FAIL_CONSUMER;
-		goto error;
-	}
 
 	if (session->kernel_session) {
 		cmd_ret = kernel_rotate_session(session);
