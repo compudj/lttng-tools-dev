3a43e53ea3c0cdbd0513eb16897b6a0d6982d5db
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2019-12-03 04:08:36 -0500

    Fix: relayd: per-pid live: no new metadata vs close
    When using lttng-live on a per-pid UST trace, the metadata stream is
    closed after an application exits. The live viewer observes that the
    stream has no more data when getting the metadata returns 0 bytes.
    
    Internally in the relay daemon, it is assumed that a metadata stream
    can be put (and thus removed) as soon as all the metadata has been sent
    to the viewer. But this is not quite right. The viewer actually needs to
    observe a 0-byte reply after receiving all the metadata in order to
    gracefully perceive the metadata stream hang up.
    
    Therefore, add a no_new_metadata_notified flag to the metadata stream
    to track whether that 0-byte message has been sent to the viewer since
    the last metadata content was sent, and postpone reclaim of the metadata
    stream until it is closed _and_ that 0-byte reply was sent to the live
    viewer.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- src/bin/lttng-relayd/live.c
+++ src/bin/lttng-relayd/live.c
@@ -1725,6 +1725,17 @@ int viewer_get_metadata(struct relay_connection *conn)
 	len = vstream->stream->metadata_received - vstream->metadata_sent;
 	if (len == 0) {
 		reply.status = htobe32(LTTNG_VIEWER_NO_NEW_METADATA);
+		/*
+		 * The live viewer considers a closed 0 byte metadata stream as
+		 * an error.
+		 */
+		if (vstream->metadata_sent > 0) {
+			vstream->stream->no_new_metadata_notified = true;
+			if (vstream->stream->closed) {
+				/* Release ownership for the viewer metadata stream. */
+				viewer_stream_put(vstream);
+			}
+		}
 		goto send_reply;
 	}
 
@@ -1772,12 +1783,6 @@ int viewer_get_metadata(struct relay_connection *conn)
 		goto error;
 	}
 	vstream->metadata_sent += read_len;
-	if (vstream->metadata_sent == vstream->stream->metadata_received
-			&& vstream->stream->closed) {
-		/* Release ownership for the viewer metadata stream. */
-		viewer_stream_put(vstream);
-	}
-
 	reply.status = htobe32(LTTNG_VIEWER_METADATA_OK);
 
 	goto send_reply;
